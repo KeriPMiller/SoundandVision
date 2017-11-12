293aada41c85515a6a6a310b082d940acf3f01dd
KeriPMiller <miller.keripatricia@gmail.com>
2017-11-12 09:30:23 -0500

    tweaked canvas for better testing

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,18 +1,27 @@
-var mic, amplitude;
+// global variables
+let mic, amp;
 
 function setup() {
-  createCanvas(windowWidth, 300);
-  mic = new p5.AudioIn()
+  createCanvas(windowWidth, windowHeight);
+  // get audio
+  mic = new p5.AudioIn();
   mic.start();
-  amplitude = new p5.Amplitude()
+  // get amplitude and set it to the mic
+  amp = new p5.Amplitude();
+  amp.setInput(mic);
 }
 
 function draw() {
-  background(65);
   micLevel = mic.getLevel();
+  let micVal = Math.floor(micLevel * 100);
 
+  // to make the bg col fluxuate fromblack to white on sound
+  let col = map(micVal, 0, 100, 0, 255);
+  background(col);
+
+  // blue ball with diagnal bottomright to top left movement
   noStroke();
-  fill(color(0, 100, 200))
+  fill(color(0, 100, 200));
   ellipse(
     constrain(0, width - micLevel * 5 * width, width),
     constrain(height - micLevel * height * 5, 0, height),
@@ -20,14 +29,16 @@ function draw() {
     10
   );
 
-  fill(color(255,204,0))
+  // larger yellow ball that moves from right to left horizontally
+  fill(color(255, 204, 0));
   ellipse(
-    constrain(0, width-micLevel * 5 * width, width),
+    constrain(0, width - micLevel * 5 * width, width),
     75,
     80,
     80
   );
 
-  stroke(150)
-  line(round(micLevel*width), 0, round(micLevel*100), height);
+  // white line top moves from left to right at an angle
+  stroke(150);
+  line(round(micLevel * width), 0, round(micLevel * 100), height);
 }
