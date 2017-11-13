e50b8963d59d7118852ce72f187abd535145a2ae
KeriPMiller <miller.keripatricia@gmail.com>
2017-11-13 12:18:49 -0500

    getting audio and emiting socket event

--- public/scripts/peerRTC.js
+++ public/scripts/peerRTC.js
@@ -1,16 +1,24 @@
 console.log('peerRTC.js connected')
+let audio;
+navigator.mediaDevices.getUserMedia({audio:true, video:false})
+.then(function (stream) {
+  audio = stream;
+  socket.emit('sendVoice', audio);
+  return audio;})
+.catch( err => console.error(err))
 
-const host = new PeerConnection('http://XXX.XXX.X.XXX:3000');
-console.log(host);
 
-host.onStreamAdded = function(e) {
-  console.log('getting in here')
-  document.body.appendChild(e.mediaElement);
-};
-
-
-const stranger = new PeerConnection('http://XXX.XXX.X.XXX:3000');
-stranger.onStreamAdded = function(e) {
-  document.body.appendChild(e.mediaElement);
-};
-stranger.startBroadcasting('host');
+// const host = new PeerConnection('http://XXX.XXX.X.XXX:3000');
+// console.log(host);
+//
+// host.onStreamAdded = function(e) {
+//   console.log('getting in here')
+//   document.body.appendChild(e.mediaElement);
+// };
+//
+//
+// const stranger = new PeerConnection('http://XXX.XXX.X.XXX:3000');
+// stranger.onStreamAdded = function(e) {
+//   document.body.appendChild(e.mediaElement);
+// };
+// stranger.startBroadcasting('host');
